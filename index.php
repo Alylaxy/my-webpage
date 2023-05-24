653dd381c5ac1caf7e2bc400e5c3e1e28d4a421f
Alylaxy <jp8nogueira@gmail.com>
2023-05-24 19:26:25 -0300

    Create class Database

--- index.php
+++ index.php
@@ -1,4 +1,33 @@
 <?php
 
+//removed router.php to test database
+//everything public cause i dont know what shouldnt be public yet :D
+
 require 'functions.php';
-require 'router.php';
+// require 'router.php';
+
+
+class Database{
+
+    public $connection;
+    public function __construct(){
+        $dsn = 'mysql:host=localhost;port=3306;dbname=my-webpage;charset=utf8mb4;user=root;';
+        
+        $this->connection = new PDO($dsn);
+    }
+    public function query($query){
+        
+        
+        $statement = $this->connection->prepare($query);
+        
+        $statement->execute();
+        
+        return $statement;
+    }
+}
+
+$db = new Database;
+
+$posts = $db->query("SELECT * FROM posts")->fetchAll(PDO::FETCH_OBJ);
+
+dd($posts);
