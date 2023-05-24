a31a0cc726a86d958b9d8e956f537da8d239588a
Alylaxy <jp8nogueira@gmail.com>
2023-05-24 19:40:16 -0300

    Create independent file for Database class

--- index.php
+++ index.php
@@ -1,30 +1,10 @@
 <?php
 
 //removed router.php to test database
-//everything public cause i dont know what shouldnt be public yet :D
 
 require 'functions.php';
 // require 'router.php';
-
-
-class Database{
-
-    public $connection;
-    public function __construct(){
-        $dsn = 'mysql:host=localhost;port=3306;dbname=my-webpage;charset=utf8mb4;user=root;';
-        
-        $this->connection = new PDO($dsn);
-    }
-    public function query($query){
-        
-        
-        $statement = $this->connection->prepare($query);
-        
-        $statement->execute();
-        
-        return $statement;
-    }
-}
+require 'Database.php';
 
 $db = new Database;
 
