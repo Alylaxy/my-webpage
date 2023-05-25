5a4319c9ec9d17b76148b3a70a4db6de48cd3d47
Alylaxy <jp8nogueira@gmail.com>
2023-05-24 23:25:39 -0300

    Add basic SQL Injection protection

--- index.php
+++ index.php
@@ -7,10 +7,11 @@ require 'functions.php';
 require 'Database.php';
 
 $config = require 'config.php';
-
-
 $db = new Database($config['database']);
 
-$posts = $db->query("SELECT * FROM posts")->fetchAll();
+$id = $_GET['id'];
+$query = "SELECT * FROM posts where id = :id";
+
+$posts = $db->query($query, [':id' => $id])->fetchAll();
 
 dd($posts);
